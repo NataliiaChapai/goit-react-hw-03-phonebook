1d607bc1fa550e43a4ad522032d4007aa93485da
NataliiaChapai <natalya.chapay@gmail.com>
2022-04-30 02:04:50 +0300

    add components Profile and Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,9 +27,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />;
-      <Statistics stats={data} />;
-        
+      <Statistics title="Upload stats" stats={data} />
+              
     </div>
   );
 };
