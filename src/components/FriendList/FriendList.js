a77e85cd4a9a96929c6e851726444de41704e1e2
NataliiaChapai <natalya.chapay@gmail.com>
2022-04-30 21:42:44 +0300

    add component 3

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import s from './FriendList.module.css';
+
+function FriendList({ friends }) {
+  return (
+    <ul className={s.friendList}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
